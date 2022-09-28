3520bd938dac37abf6fbe84079a3e2eb4068ad3a
Amine <ouas.amine@outlook.com>
2022-09-29 00:38:42 +0100

    Implement Add & Remove & LocalStorage Functionalities

---
+++ src/add-remove.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+
+export function addToList(tasksArray, newTaskDesc) {
+  const newIndex = tasksArray.length ? tasksArray.length : 0;
+  tasksArray.push({
+    index: newIndex,
+    desc: newTaskDesc,
+    completed: false,
+  });
+}
+
+export function removeFromList(tasksArray, taskInedx) {
+  tasksArray.splice(taskInedx, 1);
+}
+
+function updateIndexes(tasksArray) {
+  let cn = 0;
+  tasksArray.forEach((task) => {
+    task.index = cn;
+    cn += 1;
+  });
+}
+
+export function updateStorage(tasksArray) {
+  if (tasksArray) {
+    updateIndexes(tasksArray);
+    const storeTasksArray = JSON.stringify(tasksArray);
+    localStorage.setItem('tasks', storeTasksArray);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,34 +4,63 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import { addToList, removeFromList, updateStorage } from './add-remove.js';
 
-const tasks = [{
-  index: 0,
-  desc: 'wash dishes',
-  completed: false,
-}, {
-  index: 1,
-  desc: 'complete to do list project',
-  completed: false,
-}];
+const htmlTasksContainer = document.querySelector('ul');
+const newTaskInput = document.querySelector('input');
+let tasks = [];
 
-function displayTasks(container, tasks) {
+function displayTasks(container) {
+  container.innerHTML = '';
+  const fetchedTasks = JSON.parse(localStorage.getItem('tasks'));
+  if (fetchedTasks) {
+    tasks = fetchedTasks;
+  }
   tasks.forEach((task) => {
     const listitem = document.createElement('li');
     const checkbox = document.createElement('input');
+    const moveButt = document.createElement('div');
+    const removeButt = document.createElement('div');
     const icon = document.createElement('span');
-    icon.classList.add('setting', 'fa-solid', 'fa-ellipsis-vertical', 'fa-xl');
+    const icon2 = document.createElement('span');
+    icon.classList.add('fa-solid', 'fa-ellipsis-vertical', 'fa-lg');
+    icon2.classList.add('fa-solid', 'fa-trash-can', 'fa-lg');
+    moveButt.classList.add('move', 'setting');
+    moveButt.appendChild(icon);
+    // icon.classList.replace('fa-ellipsis-vertical', 'fa-trash-can');
+    removeButt.classList.add('remove', 'setting');
+    removeButt.appendChild(icon2);
     checkbox.type = 'checkbox';
     listitem.contentEditable = 'true';
     listitem.appendChild(checkbox);
     listitem.appendChild(document.createTextNode(task.desc));
-    listitem.appendChild(icon);
+    listitem.appendChild(moveButt);
     listitem.addEventListener('input', () => {
       task.desc = listitem.innerText;
+      updateStorage(tasks);
     });
     container.appendChild(listitem);
+    listitem.addEventListener('focus', (e) => {
+      moveButt.remove();
+      e.currentTarget.appendChild(removeButt);
+      removeButt.addEventListener('click', () => {
+        listitem.remove();
+        removeFromList(tasks, task.index);
+        updateStorage(tasks);
+      });
+    });
+    listitem.addEventListener('focusout', (e) => {
+      removeButt.remove();
+      e.currentTarget.appendChild(moveButt);
+    });
   });
 }
 
-const htmlTasksContainer = document.querySelector('ul');
 displayTasks(htmlTasksContainer, tasks);
+newTaskInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addToList(tasks, e.currentTarget.value);
+    updateStorage(tasks);
+    displayTasks(htmlTasksContainer);
+  }
+});
